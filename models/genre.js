eebfe14e2c213eb212f1bc141df9a10c16c2c586
Mergen Delger <part1zon20@gmail.com>
2019-11-30 19:20:28 +0800

    Fist version of app;ication moved into github

---
+++ models/genre.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema (
+    {
+        name: {type: String, required: true, min: 3, max: 100}
+        
+    }
+);
+
+//virtual for genre
+GenreSchema
+.virtual('url')
+.get(function () {
+    return '/catalog/genre/'+ this._id;
+});
+
+//exprot model
+module.exports = mongoose.model('Genre', GenreSchema);
