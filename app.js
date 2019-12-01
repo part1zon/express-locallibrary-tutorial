be41d259b9a7ba8829eca5b076e627142d67ba23
Mergen Delger <part1zon20@gmail.com>
2019-12-01 12:03:04 +0800

    Added files and chenges required for deployment to heroku

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" 
 var app = express();
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://Mergen:Nuutsug20@cluster0-imbyo.gcp.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://Mergen:Nuutsug20@cluster0-imbyo.gcp.mongodb.net/local_library?retryWrites=true&w=majority'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
